e3510c20b9b4da223bdfb406339e6c11a53c5425
atchee <36660862+atchee@users.noreply.github.com>
2018-04-22 18:46:54 +0200

    3rd try

--- cours4/script.js
+++ cours4/script.js
@@ -22,7 +22,7 @@ welcome();
 
 function loadChallenges() {
   $.ajax(
-        {url : 'https://api.myjson.com/bins/dkim3',
+        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
         type: 'GET',
         dataType: 'json'}
   )
