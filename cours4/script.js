ac17b9ae393e8546e435b3b8761943d872719c00
atchee <36660862+atchee@users.noreply.github.com>
2018-04-22 21:33:58 +0200

    7th try

--- cours4/script.js
+++ cours4/script.js
@@ -1,23 +1,22 @@
 $( document ).ready(function() {
-  alert('Hello');
-  });
-function getUsername() {
 
-    //affiche une boite de dialogue
-    var  user = prompt("Veuillez rentrez votre nom:", "");
-
-    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
-    return user;
-}
-
-
-function welcome() {
+ function welcome() {
     var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez prêt à relever de nouveaux défis ! </h4>');
 
-}
+};
+
 // Appel la fonction
 welcome();
+
+$("iframe").addClass('iframe');
+
+	function getUsername() {
+    	//affiche une boite de dialogue
+    	var  user = prompt("Veuillez rentrez votre nom:", "");
+    	// la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    	return user
+}
 
 
 function loadChallenges() {
@@ -40,17 +39,18 @@ function showChallenge(liste) {
 
   for ( var i=0; i < liste.length; i++) {
     data+='<div class="defi">';
+
     var challenge=liste[i];
 
     data+='<h2>'+challenge.nom+'</h2>';
     data+='<p>'+challenge.description+'</p>';
-    data+='<iframe width="364" height="204" src="'+challenge.youtube+'" frameborder="0" allowfullscreen></iframe>';
+    data+='<iframe width="364" height="204" src="'+challenge.youtube+'" frameborder="0" allowfullscreen=""></iframe>'
     data+="</div>";
   }
   $("#defis").html(data);
 
 }
-//Charge le JSON des challenges
+
 loadChallenges();
 
-end;
+});
